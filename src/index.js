61e5723bed79b8b00c2e8a3e31230e27a3085ea2
Hamdan H <hamdan@tokodistributor.com>
2020-10-15 13:18:13 +0700

    eslint extends config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const a = () => console.log('hello world')
 
 typeof a === 'string'
+
+if (!('serviceWorker' in navigator)) {
+     // no browser support
+}
+
+const greeting = 'hello';
+console.log(`${greeting} world!`);
